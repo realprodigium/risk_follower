a0ee2cd30f9caea51c9f96dd4027232bade01d3b
franchisium <scarrdona@gmail.com>
2025-12-17 15:45:25 -0500

    endpoints records, get & post, sqlite test | 17/12/25

--- app/db.py
+++ app/db.py
@@ -5,14 +5,11 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL')
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine("sqlite:///payloads.db", connect_args={"autocommit": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
-    
     db = SessionLocal()
     try:
         yield db
--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from app.db import get_db, Base, engine
 from sqlalchemy.orm import Session
 from app.database import models
 from app.database import schemas
+from typing import List
 
 
 Base.metadata.create_all(bind=engine)
@@ -12,15 +13,15 @@ app = FastAPI(title="CO2 Monitoring Backend")
 def home():
     return {"message": "Backend is running"}
 
-@app.get('/records/', response_model=schemas.Record)
-async def read_records(record: schemas.Record, db:Session=Depends(get_db)):
-    db_record = db.query(models.Records).filter(models.Records.timestamp == record.timestamp).first()
-    if not db_record:
+@app.get('/records/', response_model=List[schemas.Record])
+def read_records(db:Session=Depends(get_db)):
+    db_records = db.query(models.Records).all()
+    if not db_records:
         raise HTTPException(status_code=404, detail="Record not found")
-    return db_record
+    return db_records
 
 @app.post('/records/', response_model=schemas.Record)
-async def create_record(record: schemas.RecordCreate, db:Session=Depends(get_db)):
+def create_record(record: schemas.RecordCreate, db:Session=Depends(get_db)):
     if db.query(models.Records).filter(models.Records.timestamp == record.timestamp).first():
         raise HTTPException(status_code=400, detail="Record with this timestamp already exists")
     new_record = models.Records(**record.model_dump()) #.dict deprecated
