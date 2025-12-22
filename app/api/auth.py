94754017209b74bf0b7b1243e684561d4ad3135e
franchisium <scarrdona@gmail.com>
2025-12-22 12:04:04 -0500

    logica Tokenizer para login

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,24 +1,32 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from typing import Annotated
-from app.services import auth_services as aS
+from app.services import auth_services
 from app.database import schemas, models
 from app.db import get_db
 from sqlalchemy.orm import Session
+import os
+from datetime import timedelta
+from dotenv import load_dotenv
 
 
-
+load_dotenv()
 router = APIRouter()
 
 @router.post('/token', response_model=schemas.Token, tags=['auth'])
 async def login_for_access_token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
     db:Session = Depends(get_db)):
-    user = aS.authenticate_user(db, form_data.username, form_data.password)
+    user = auth_services.authenticate_user(db, form_data.username, form_data.password)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail='Incorrect username or password',
             headers={'WWW-Authenticate': 'Bearer'},
         )
+    access_token_expire = timedelta(minutes=int(os.getenvv('ACCESS_TOKEN_EXPIRE_MINUTES')))
+    access_token = auth_services.create_access_token(
+        data={'sub': user.username}, expires_delta=access_token_expire
+    )
+    return schemas.Token(access_token=access_token, token_type='bearer')
     
